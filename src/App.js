1b1073a636764842eee38f936ea4a3127b66ebb1
programarf <programarf@gmail.com>
2022-04-06 15:59:22 -0500

    Added my project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import React, { useEffect, useRef } from 'react';
+import { gsap } from "gsap";
 import './App.css';
 
+function Box({ children }) {
+  return <div className="box">{children}</div>;
+}
+
+function Container({ children }) {
+  return <div><Box>Nested Box</Box></div>;
+}
+
 function App() {
+  const el = useRef();
+  const q = gsap.utils.selector(el);
+
+  useEffect(() => {
+
+    // Target any descendant with the class of .box - no matter how far down the descendant tree. Uses el.current.querySelectorAll() internally
+    gsap.to(q(".box"), {
+      x: 100,
+      stagger: 0.33,
+      repeat: -1,
+      repeatDelay: 1,
+      yoyo: true
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" ref={el}>
+      <Box>Box</Box>
+      <Container />
+      <Box>Box</Box>
     </div>
   );
 }
